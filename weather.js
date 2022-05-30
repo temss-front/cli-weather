c0ed6abf835323af37309552d132b9e80e02ca7a
wibimipi <i@arborisov.ru>
2022-05-30 23:51:09 +0300

    added storage service

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import {getArgs} from "./helpers/getArgs.js";
 import {printError, printSuccess, printHelp} from "./services/log.service.js";
+import {saveKeyValue, getKeyValue} from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue("token", token)
+        printSuccess("Токен успешно сохранён")
+    } catch (e) {
+        printError(e.message)
+    }
+}
 
 const init = () => {
     const args = getArgs(process.argv)
@@ -12,7 +22,7 @@ const init = () => {
             //save city
         }
         if (args.t) {
-            //save token
+            return saveToken(args.t)
         }
         // output weather
     } catch (e) {
